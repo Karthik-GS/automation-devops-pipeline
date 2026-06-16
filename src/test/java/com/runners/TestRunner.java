d05f37abf1311110c3e9bda813fc77563e8bb9ea
Karthik-GS <karthikgsuvarna@gmail.com>
2026-06-16 17:39:22 +0530

    updated glue in test runner

--- src/test/java/com/runners/TestRunner.java
+++ src/test/java/com/runners/TestRunner.java
@@ -6,10 +6,15 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
+//@CucumberOptions(
+//        features = "src/test/resources/features",
+//        glue = {"com.karthik.config.stepdefinitions"},
+//        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"} // Generates Allure lifecycle json log
+//)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"com.karthik.config.stepdefinitions"},
-        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"} // Generates Allure lifecycle json log
+        glue = {"com.config.stepdefinitions"},
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
 public class TestRunner {
 }
